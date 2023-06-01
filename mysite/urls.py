ea2a1df649089ee1d882e1375116700cc820d14c
fkdjswjd1 <85562779+fkdjswjd1@users.noreply.github.com>
2023-06-01 09:58:01 +0900

    Update urls.py
    change

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('users/', include('users.urls')),
+   path('users/', include('users.urls')), # include 임폴트 해야함
    path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
